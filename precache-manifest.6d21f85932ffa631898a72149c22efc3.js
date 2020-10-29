05120a003806d13449701cb7f8951a2f2ce56f05
Dan Sarich <dsarich33@gmail.com>
2020-10-29 09:54:52 -0600

    Updates

---
+++ precache-manifest.6d21f85932ffa631898a72149c22efc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4084094324467503933a37f22b1f565",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7a7ddf7ced19b7cf60e0",
+    "url": "/portfolio/static/css/main.62bf418b.chunk.css"
+  },
+  {
+    "revision": "9721f8c3013214fae7c2",
+    "url": "/portfolio/static/js/2.2c37d16e.chunk.js"
+  },
+  {
+    "revision": "d67593149659a61039b34c3ef5fe8c31",
+    "url": "/portfolio/static/js/2.2c37d16e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7ddf7ced19b7cf60e0",
+    "url": "/portfolio/static/js/main.a4d7aaf5.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "f71ba3e8a7a5d71775d6bee6e56c53cc",
+    "url": "/portfolio/static/media/avatar.f71ba3e8.png"
+  }
+]);
